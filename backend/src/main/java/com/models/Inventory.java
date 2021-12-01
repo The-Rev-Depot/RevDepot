3cceaa62f8b1ea43e248180179e60b874865ad2b
Zone50o <carr.andrew29@gmail.com>
2021-12-01 12:31:51 -0500

    Established table relationships

--- backend/src/main/java/com/models/Inventory.java
+++ backend/src/main/java/com/models/Inventory.java
@@ -1,12 +1,11 @@
 package com.models;
 
-import java.sql.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -25,8 +24,8 @@ public class Inventory {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name ="inventory_id")
 	private int inventoryId;
-	@Column(name ="product_id")
-	private int productId;
+	@OneToOne(mappedBy = "productId")
+	private Product product;
 	private double quantity;
 	
 }
