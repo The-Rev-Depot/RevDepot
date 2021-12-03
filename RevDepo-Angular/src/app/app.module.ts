25b3fe2dc8f271da62a47a8cc576b24f78835467
Freddie-TadeoL <geeps_1999@live.com>
2021-12-03 13:40:06 -0800

    ng installed and update to set up webpage

--- RevDepo-Angular/src/app/app.module.ts
+++ RevDepo-Angular/src/app/app.module.ts
@@ -29,7 +29,7 @@ import { NavbarComponent } from './component/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
