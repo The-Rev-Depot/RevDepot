9e56e69b37c4ff3f4ff9191f42777153f48b081c
Ryan Moss <rymoss.9270@gmail.com>
2021-12-03 15:17:43 -0800

    fixed inventory update method

--- backend/src/main/java/com/controllers/InventoryController.java
+++ backend/src/main/java/com/controllers/InventoryController.java
@@ -7,4 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "inventory")
 public class InventoryController {
 
+	
 }
--- backend/src/test/java/com/services/InventoryServiceTest.java
+++ backend/src/test/java/com/services/InventoryServiceTest.java
@@ -33,8 +33,8 @@ public class InventoryServiceTest {
 		// Arrange
 		Inventory expectedInventory = new Inventory();
 		
-		when(inventoryDao.findById(expectedInventory.getInventoryId()))
-		.thenReturn(Optional.of(expectedInventory));
+		when(inventoryDao.getById(expectedInventory.getInventoryId()))
+		.thenReturn(expectedInventory);
 		
 		when(inventoryDao.save(expectedInventory)).thenReturn(expectedInventory);
 		
@@ -42,7 +42,7 @@ public class InventoryServiceTest {
 		Inventory actualInventory = inventoryService.updateInventory(expectedInventory);
 		
 		// Assert
-		verify(inventoryDao, times(1)).findById(expectedInventory.getInventoryId());
+		verify(inventoryDao, times(1)).getById(expectedInventory.getInventoryId());
 		verify(inventoryDao, times(1)).save(expectedInventory);
 		assertEquals(actualInventory, expectedInventory);
 		
