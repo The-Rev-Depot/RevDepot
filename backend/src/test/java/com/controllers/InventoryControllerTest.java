0a970b45fcd4777ae7f148162aefc46c1e7bca2b
Kienen <Kienen@users.noreply.github.com>
2021-12-06 08:33:25 -0800

    updateInventory controller function

---
+++ backend/src/test/java/com/controllers/InventoryControllerTest.java
@@ -0,0 +1,42 @@
+package com.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.models.Inventory;
+import com.services.InventoryService;
+import com.services.InventoryServiceImpl;
+
+@SpringBootTest
+public class InventoryControllerTest {
+	
+	InventoryController inventoryController;
+
+	@Mock
+	InventoryService inventoryService;
+	
+	@BeforeEach
+	void setUp() {
+		inventoryController = new InventoryController(inventoryService);
+	}
+	
+	
+	@Test
+	void updateInventoryTest() {
+		// Arrange
+		Inventory expectedInventory = new Inventory();
+		
+		// Act
+		Inventory actualInventory = inventoryController.updateInventory(expectedInventory);
+		
+		// Assert
+		verify(inventoryService, times(1)).updateInventory(expectedInventory);
+	}
+}
