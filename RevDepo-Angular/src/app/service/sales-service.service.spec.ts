d2bd8911b52bb82c001129d94b87e9a45ee818a1
jjlrevature <jesse.lauesen@revature.net>
2021-12-07 15:11:35 -0600

    tried new testing, didnt work

--- RevDepo-Angular/src/app/service/sales-service.service.spec.ts
+++ RevDepo-Angular/src/app/service/sales-service.service.spec.ts
@@ -8,7 +8,8 @@ import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 fdescribe('SalesServiceService', () => {
   let service: SalesServiceService;
-
+  let httpTestCont: HttpTestingController;
+  const httpClient = jasmine.createSpyObj('HttpClient',['get']);
   
 
   let fetchedData:any = []
@@ -18,7 +19,8 @@ fdescribe('SalesServiceService', () => {
       imports: [HttpClientTestingModule],
       providers: [SalesServiceService]
     });
-    service = TestBed.inject(SalesServiceService);
+    service = TestBed.get(SalesServiceService);
+    httpTestCont = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
@@ -26,37 +28,41 @@ fdescribe('SalesServiceService', () => {
   });
 
 
-
+  it('should get all products on sale', () => {
+    
+    httpClient.get.and.returnValue(200);
+    expect(service.getAllItemsOnSale()).toBe(200 as any);
+  })
 
   // this aint working yet, returned objects are undefined
-  it('first item should have a isOnSale value of 10',
-    inject(
-      [HttpTestingController, SalesServiceService],
-      (httpMock: HttpTestingController, service: SalesServiceService) => {
+  // it('first item should have a isOnSale value of 10',
+  //   inject(
+  //     [HttpTestingController, SalesServiceService],
+  //     (httpMock: HttpTestingController, service: SalesServiceService) => {
         
-        const mockObject: Product = {
-          productId: 0,
-          productName: "test name",
-          description: "test description",
-          picUrl: "com",
-          productPrice: 10.99,
-          category: "category",
-          isOnSale: 10,
-          productRating:5
-        }
+  //       const mockObject: Product = {
+  //         productId: 0,
+  //         productName: "test name",
+  //         description: "test description",
+  //         picUrl: "com",
+  //         productPrice: 10.99,
+  //         category: "category",
+  //         isOnSale: 10,
+  //         productRating:5
+  //       }
 
-        service.getAllItemsOnSale();
-        expect(service.allDeals[0]).toEqual(mockObject);
+  //       service.getAllItemsOnSale();
+  //       expect(service.allDeals[0]).toEqual(mockObject);
 
-        // service.getAllItemsOnSale().subscribe(
-        //   (response) => {
-        //     fetchedData = response;
-        //     console.log(fetchedData);
-        //     expect(fetchedData[0]);
+  //       // service.getAllItemsOnSale().subscribe(
+  //       //   (response) => {
+  //       //     fetchedData = response;
+  //       //     console.log(fetchedData);
+  //       //     expect(fetchedData[0]);
             
-        //   }
-        // )
-      }
-    )  
-  )
+  //       //   }
+  //       // )
+  //     }
+  //   )  
+  // )
 });
