ee263fe55c2488c7a44607b37ca4c9e2e26b7ea1
Kienen <Kienen@users.noreply.github.com>
2021-12-07 14:44:58 -0800

    Frontend pages begun

--- RevDepo-Angular/src/app/service/cart.service.ts
+++ RevDepo-Angular/src/app/service/cart.service.ts
@@ -31,6 +31,18 @@ export class CartService {
      return total;
    }
 
+   getTotalQty(): number {
+     let total: number =0;
+
+     this.cart?.items.forEach(
+      (value)=>{
+       total +=  value.quantity;
+      }
+    );
+
+    return total;
+   }
+
    checkoutCart(): Observable<IItem[]> {
     const httpPost = {
       headers: new HttpHeaders({
@@ -57,9 +69,9 @@ export class CartService {
         {
           itemId: 0, quantity: 1, product: {
             productId: 0,
-            productName: "prod1",
+            productName: "Computer Tower Stand",
             description: "",
-            picUrl: "",
+            picUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg",
             productPrice: 15,
             productRating: 0,
             category: "",
@@ -69,9 +81,9 @@ export class CartService {
         {
           itemId: 1, quantity: 3, product: {
             productId: 0,
-            productName: "prod2",
+            productName: "Renpho Powerful Portable Massage Gun",
             description: "",
-            picUrl: "",
+            picUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg",
             productPrice: 20,
             productRating: 0,
             category: "",
@@ -81,9 +93,9 @@ export class CartService {
         {
           itemId: 2, quantity: 5, product: {
             productId: 0,
-            productName: "prod3",
+            productName: "Rollerblade Zetrablade Men's Adult Fitness Inline Skate",
             description: "",
-            picUrl: "",
+            picUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg",
             productPrice: 50,
             productRating: 0,
             category: "",
