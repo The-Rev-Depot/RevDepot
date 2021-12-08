2de99b4e89d8cae8f5aa33e7b5830db72f2cd88f
Roel Crodua <roelcrodua@yahoo.com>
2021-12-08 13:38:54 -0500

    Added Jasmine testing

--- RevDepo-Angular/src/app/app.module.ts
+++ RevDepo-Angular/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
@@ -29,7 +28,7 @@ import { NavbarComponent } from './component/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
