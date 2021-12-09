f9140fd570c37e33680905427393c672a2da1328
Michael Keefer <mike.keefer@revature.net>
2021-12-09 08:37:44 -0800

    12/9 added register account ts and html

--- RevDepo-Angular/src/app/component/register/register.component.ts
+++ RevDepo-Angular/src/app/component/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from 'src/app/model/user';
+import { UserServiceService } from 'src/app/service/user-service.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userServ: UserServiceService, private router:Router) { }
+
+  userAccount: IUser=
+    {
+    userId: 0,
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    urlProPic: '',
+    birthday: '',
+  }
+
 
   ngOnInit(): void {
+  }
+
+  registerUser() {
+
+    this.userServ.addUserRequest(this.userAccount).subscribe(data => { console.log(data) });
+
   }
 
 }
