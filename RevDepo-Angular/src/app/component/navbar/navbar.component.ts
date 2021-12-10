38fb81ee2daa6b4ff584f75d751e7fd19bcfaf68
John Benson <john.benson@revature.net>
2021-12-10 10:58:45 -0700

    12/10/2021 11am added basic functionality to toolbar

--- RevDepo-Angular/src/app/component/navbar/navbar.component.ts
+++ RevDepo-Angular/src/app/component/navbar/navbar.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { ICart } from 'src/app/model/cart';
 import { IProduct } from 'src/app/model/product';
+import { CartService } from 'src/app/service/cart.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -8,63 +12,39 @@ import { IProduct } from 'src/app/model/product';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  products: IProduct[] = [
-     {
-      productId: 1,
-      productName: "t-shirt",
-      description: "code like a boss",
-      picUrl: "https://i.ebayimg.com/thumbs/images/g/yzMAAOSwaHZgy1VV/s-l300.jpg",
-      productPrice: 23.99,
-      productRating: 3,
-      category: "clothing",
-      isOnSale: 25,
-      }, 
-      {      
-      productId: 1,
-      productName: "t-shirt",
-      description: "code like a boss",
-      picUrl: "https://i.ebayimg.com/thumbs/images/g/yzMAAOSwaHZgy1VV/s-l300.jpg",
-      productPrice: 23.99,
-      productRating: 3,
-      category: "clothing",
-      isOnSale: 25
-  },
-  {
-    productId: 1,
-    productName: "t-shirt",
-    description: "code like a boss",
-    picUrl: "https://i.ebayimg.com/thumbs/images/g/yzMAAOSwaHZgy1VV/s-l300.jpg",
-    productPrice: 23.99,
-    productRating: 3,
-    category: "clothing",
-    isOnSale: 25,
-    }, 
-    {      
-    productId: 1,
-    productName: "t-shirt",
-    description: "code like a boss",
-    picUrl: "https://i.ebayimg.com/thumbs/images/g/yzMAAOSwaHZgy1VV/s-l300.jpg",
-    productPrice: 23.99,
-    productRating: 3,
-    category: "clothing",
-    isOnSale: 25
-}
-
-
-  ];
+  cart?: ICart;
+  totalPrice?: number;
   cartIsEmpty = false;
   loggedIn = true;
   @ViewChild(MatSidenav) sidenav!: MatSidenav;
   events: string[] = [];
   opened: boolean = false;
-  constructor() { }
+
+
+  constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
+    this.cart = this.cartService.getCart();
+    this.totalPrice = this.cartService.getTotalPrice();
+
   }
 
   toggleSideNav(){
     this.opened != this.opened;
     this.sidenav.toggle();
+  }
+
+  removeItem(){
+    console.log("item removed");
+    console.log(this.cart?.items);
+  }
+
+  checkoutRoute(){
+    this.router?.navigateByUrl('/checkout');
+  }
+
+  homeRoute(){
+    this.router?.navigateByUrl('/display-products');
   }
 
 }
