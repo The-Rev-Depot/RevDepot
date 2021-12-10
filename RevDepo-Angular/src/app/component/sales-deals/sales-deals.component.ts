e8f870ec1107ce1e4219d5e52ee5ac4ede3693a0
jjlrevature <jesse.lauesen@revature.net>
2021-12-10 15:55:49 -0600

    component update

--- RevDepo-Angular/src/app/component/sales-deals/sales-deals.component.ts
+++ RevDepo-Angular/src/app/component/sales-deals/sales-deals.component.ts
@@ -14,11 +14,76 @@ export class SalesDealsComponent implements OnInit {
   productsOnSale:any = []
   productsOnSaleByCategory:any = []
 
+  shirtPro =
+  {
+
+    productId: 1,
+    productName: "card",
+    description: "string",
+    picUrl: "string",
+    productPrice: 15,
+    category: "string",
+    isOnSale: 1
+
+  }
+  shirtPro1 =
+  {
+
+    productId: 2,
+    productName: "dispalycard" ,
+    description: "string",
+    picUrl: "string",
+    productPrice: 15,
+    category: "string",
+    isOnSale: 1
+
+  }
+  shirtPro2 =
+  {
+
+    productId: 3,
+    productName: "string" ,
+    description: "string",
+    picUrl: "string",
+    productPrice: 15,
+    category: "string",
+    isOnSale: 1
+
+  }
+  shirtPro3 =
+  {
+
+    productId: 3,
+    productName: "string" ,
+    description: "string",
+    picUrl: "string",
+    productPrice: 15,
+    category: "string",
+    isOnSale: 1
+
+  }
+  shirtPro4 =
+  {
+
+    productId: 3,
+    productName: "string" ,
+    description: "string",
+    picUrl: "string",
+    productPrice: 15,
+    category: "string",
+    isOnSale: 1
+
+  }
+
+
   constructor(private service:SalesServiceService) { }
 
   ngOnInit(): void {
     this.productsOnSale = this.getProductsOnSale();
     this.productsOnSaleByCategory = this.getProductsOnSaleByCategory();
+
+    // delete next line for production
+    this.productsOnSale = [this.shirtPro,this.shirtPro1,this.shirtPro2,this.shirtPro3,this.shirtPro4]
   }
 
   private getProductsOnSale() {
