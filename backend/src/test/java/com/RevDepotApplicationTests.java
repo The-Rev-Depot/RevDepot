ee6049932f9b25ef3336dfbd35671f63e9129be6
Najee Mendes <nmendes@fandm.edu>
2021-12-13 17:00:09 -0500

    JUnit test working again

--- backend/src/test/java/com/RevDepotApplicationTests.java
+++ backend/src/test/java/com/RevDepotApplicationTests.java
@@ -28,22 +28,23 @@ class RevDepotApplicationTests {
 	@Autowired
 	private InventoryDao inventoryDao;
 
-	Product product = new Product(1, "rev pro shirt","shirt with Revature logo", "www.google.com",
-			15, newFloat, "apparel", 0);
+	Product product = new Product(1, "rev pro shirt","shirt with revature logo", "https://i.ebayimg.com/thumbs/images/g/yzMAAOSwaHZgy1VV/s-l300.jpg ",
+			15, newFloat, "apparel", 1);
+	
 	
 	//for some reason this test is not passing, the list of products being requested is out of order...
-//	@Test
-//	@DisplayName("Testing Product Repository")
-//	public void testProducts() {
-//		
-//		List<Product> products = (List<Product>)productDao.findAll();
-//		
-//		System.out.println(products.get(0));
-//		System.out.println(product);
-//		System.out.println(products);
-//				
-//		assertEquals(product, products.get(0));
-//	}
+	@Test
+	@DisplayName("Testing Product Repository")
+	public void testProducts() {
+		
+		List<Product> products = (List<Product>)productDao.findAll();
+		
+		System.out.println(products.get(0));
+		System.out.println(product);
+		System.out.println(products);
+				
+		assertTrue(products.contains(product));
+	}
 	
 	@Test
 	@DisplayName("Testing Inventory Repository")
@@ -55,7 +56,7 @@ class RevDepotApplicationTests {
 		System.out.println(inventoryList.get(0));
 		System.out.println(inventory);
 				
-		assertEquals(inventory, inventoryList.get(0));
+		assertTrue(inventoryList.contains(inventory));
 	}
 
 }
