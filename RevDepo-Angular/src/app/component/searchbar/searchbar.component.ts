a0f176ac2de380b85bbdf3e3f71834700cbd09c1
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2021-12-14 09:06:03 -0800

    few updates to the searchbar .ts and .html files

--- RevDepo-Angular/src/app/component/searchbar/searchbar.component.ts
+++ RevDepo-Angular/src/app/component/searchbar/searchbar.component.ts
@@ -18,9 +18,10 @@ export class SearchbarComponent implements OnInit {
   public product : IProduct[] | any ;
   
 
+  // early object used to test the functions below
   mockProduct : any = [{
     productId : 1,
-    productName : "TEST Revature Stickers",
+    productName : "",
     description : "A set of stylish Revature Stickers!",
     picURL : "sticker.url...",
     productPrice : 5 ,
@@ -29,7 +30,6 @@ export class SearchbarComponent implements OnInit {
     isOnSale : 1 
 
   }]
-  searchList: any;
 
   
 
@@ -42,45 +42,55 @@ export class SearchbarComponent implements OnInit {
   public searchText : string = "";
 
 
-  public letterCheck(element:any, index:any, array:any, text:string) {
-    // element is the productName as a string
-    let count:number = 0;
-    let output: string = "NOT ENOUGH LETTERS MATCH";
+  // public letterCheck(element:any, index:any, array:any, text:string) {
+  //   // element is the productName as a string
+  //   let count:number = 0;
+  //   let output: string = "NOT ENOUGH LETTERS MATCH";
 
-    console.log(`letterCheck element: ${element}`);
-
-
-    console.log(`letterCheck text: ${text}`);
-
-    let name : string = element.productName;
-    console.log(`letterCheck name: ${name}`);
-    console.log(`letterCheck text: ${text}`);
+  //   console.log(`letterCheck element: ${element}`);
 
 
+  //   console.log(`letterCheck text: ${text}`);
 
-    for (let i = 0; i<name.length; i ++) {
-      for (let s = 0 ; s<text.length; s++) {
-        if (text.toLowerCase().charAt(s) == name.toLowerCase().charAt(i)) {
-          count ++ ;
+  //   let name : string = element.productName;
+  //   console.log(`letterCheck name: ${name}`);
+  //   console.log(`letterCheck text: ${text}`);
 
-        }
-      }
-    }
+
+
+  //   for (let i = 0; i<name.length; i ++) {
+  //     for (let s = 0 ; s<text.length; s++) {
+  //       if (text.toLowerCase().charAt(s) == name.toLowerCase().charAt(i)) {
+  //         count ++ ;
+
+  //       }
+  //     }
+  //   }
     
-    if (count > 0) {
-      output = element;
+  //   if (count > 0) {
+  //     output = element;
 
-    }
+  //   }
 
-    return output;
+  //   return output;
 
-  }
+  // }
 
+
+  /**
+   * This function takes in an array and a string of text and returns a new array
+   * which only contains elements that reflect the given string of text.
+   * 
+   * @param array This is any given product array which has the property of array.productName.
+   * @param text This is the search text which will be used to filter the original array and return values that contain this text. 
+   * @returns A new array is returned only with values that contain the input text.
+   */
   public checkLetter(array : any, text:string) {
-    console.log(`array.productName: ${array.productName}`);
+    // console.log(`checkLetter|array.productName: ${array.productName}`);
     return array.filter(
       function(item:any){
-        return item.productName.toLowerCase().indexOf(text.toLowerCase()) !== -1
+        // input text is made lowercase and all the blank spaces are removed.
+        return item.productName.toLowerCase().indexOf(text.toLowerCase().replace(/\s/g,"")) !== -1
         
       })
 
@@ -88,36 +98,28 @@ export class SearchbarComponent implements OnInit {
 
 
   /**
-     * 
-     * This function will take in a search text as a string and based on that string,  
-     * it will filter through the list of products and return a new list of products
-     * with matching text. 
-     * 
-     * @param search This is a keyword the user enters in order to filter through the list
-     * of products
-     * 
-     */
+   * This functions takes in a string of text and filters the product array in order to 
+   * help the searchbar feature work. 
+   * 
+   * @param text This is the search text which will be used to filter the original array and return values that contain this text. 
+   * 
+   * @returns Returns an empty array if there is no input text from the search bar or if 
+   * the text does not match any of the product items names. Will return a filtered list of
+   * items if the input text matches any part of the procuct's name. 
+   * 
+   */
 
   public filteredList(text:string) : any {
 
     if (text == ""){
-      console.log(`text is ""`)
-      console.log(this.product);
-
-      return this.product;
+      return [];
 
     } else if (text != ""){
-      console.log(`text has some input: ${text}`);
-      // console.log(this.product.filter(this.letterCheck));
-      // return this.product.filter(this.letterCheck);
+      // console.log("test: ",this.checkLetter(this.product, text));
       return this.checkLetter(this.product, text);
     }
 
   }
-
-  public fList: any = this.filteredList(this.searchText) ;
-  
-
 
 
   ngOnInit(): void {
