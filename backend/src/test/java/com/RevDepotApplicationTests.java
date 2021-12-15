54f4164ca0e508fdde9f62cc9455b0a2f185df5b
Freddie-TadeoL <geeps_1999@live.com>
2021-12-15 13:05:24 -0800

    Update RevDepotApplicationTests.java

--- backend/src/test/java/com/RevDepotApplicationTests.java
+++ backend/src/test/java/com/RevDepotApplicationTests.java
@@ -20,7 +20,7 @@ import com.models.Product;
 @SpringBootTest
 class RevDepotApplicationTests {
 	
-	float newFloat = (float) 4.5;
+	float newFloat = (float) 4.3;
 
 	@Autowired
 	private ProductDao productDao;
@@ -28,10 +28,8 @@ class RevDepotApplicationTests {
 	@Autowired
 	private InventoryDao inventoryDao;
 
-	Product product = new Product(1, "rev pro shirt","shirt with revature logo", "https://i.ebayimg.com/thumbs/images/g/yzMAAOSwaHZgy1VV/s-l300.jpg ",
-			15, newFloat, "apparel", 1);
-	INSERT INTO product (product_name, product_description, pic_url, product_price, product_rating, product_category, sale_id) VALUES
-	('T-Shirt - Grey', 'Grey T-shirt with the phrase "GIVE LIKE A BOSS" on front. Revature Logo on back. Various sizes.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuoxnmro43rbVWEgIHsThMV1grQVuiH5wWTQ&usqp=CAU', 14.99, 4.3, 'Apparel', 0);
+	Product product = new Product(1, "T-Shirt - Grey","Grey T-shirt with the phrase \"GIVE LIKE A BOSS\" on front. Revature Logo on back. Various sizes.", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuoxnmro43rbVWEgIHsThMV1grQVuiH5wWTQ&usqp=CAU",
+			14.99, newFloat, "Apparel", 0);
 	
 	
 	//for some reason this test is not passing, the list of products being requested is out of order...
@@ -51,11 +49,11 @@ class RevDepotApplicationTests {
 	@Test
 	@DisplayName("Testing Inventory Repository")
 	public void testInventory() {
-		Inventory inventory = new Inventory(4, product,300);
+		Inventory inventory = new Inventory(2, product,30);
 		
 		List<Inventory> inventoryList = (List<Inventory>)inventoryDao.findAll();
 		
-		System.out.println(inventoryList.get(0));
+		System.out.println(inventoryList.get(1));
 		System.out.println(inventory);
 				
 		assertTrue(inventoryList.contains(inventory));
