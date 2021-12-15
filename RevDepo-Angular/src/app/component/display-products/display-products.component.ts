9ed23ef84a24be738463cc323a12ddb8a16d50b1
Freddie-TadeoL <geeps_1999@live.com>
2021-12-15 10:32:44 -0800

    display

--- RevDepo-Angular/src/app/component/display-products/display-products.component.ts
+++ RevDepo-Angular/src/app/component/display-products/display-products.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DisplayProductsComponent implements OnInit {
 
   //category headers that appear in categories section
-  Apparel = "apparel"
+  Apparel = "Apparel"
 
   Stationary =  "stationary"
 
